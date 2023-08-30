f5520915d562baf6694943ef6d3ba21920c709de
张艺璇 <635010314@qq.com>
2023-08-30 13:52:22 +0800

    text2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref } from "vue";
 
-defineProps<{ msg: string }>()
+defineProps<{ msg: string }>();
 
-const count = ref(0)
+const count = ref(0);
 </script>
 
 <template>
@@ -12,7 +12,7 @@ const count = ref(0)
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit
+      Edit2
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
