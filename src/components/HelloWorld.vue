cad213f353fc1605aa2da7499744d62f181bbaa3
张艺璇 <635010314@qq.com>
2023-08-30 14:08:31 +0800

    test5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,7 @@ const count = ref(0);
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
     <p>
-      Edit23
+      Edit233
       <code>components/HelloWorld.vue</code> to test HMR
     </p>
   </div>
