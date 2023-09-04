d17c734351d9aed3860d0f559f13389f46944416
张艺璇 <635010314@qq.com>
2023-09-04 18:03:44 +0800

    feature(custom): test2

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@ import HelloWorld from "./components/HelloWorld.vue";
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-    <div>11</div>
+    <div>1331</div>
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
