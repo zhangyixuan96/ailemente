9fb51fbb0cbe355d17396d723372368a92573ab5
张艺璇 <635010314@qq.com>
2023-09-04 15:19:57 +0800

    test: test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,28 +2,32 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  extends: ['standard-with-typescript', 'plugin:vue/vue3-essential'],
+  extends: [
+    "standard-with-typescript",
+    "plugin:vue/vue3-essential",
+    "plugin:prettier/recommended",
+  ],
   overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: ['.eslintrc.{js,cjs}'],
+      files: [".eslintrc.{js,cjs}"],
       parserOptions: {
-        sourceType: 'script'
-      }
-    }
+        sourceType: "script",
+      },
+    },
   ],
-  parser: 'vue-eslint-parser',
+  parser: "vue-eslint-parser",
   parserOptions: {
-    parser: '@typescript-eslint/parser', // 新增
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: ['tsconfig.json'], // 新增
-    extraFileExtensions: ['.vue']
+    parser: "@typescript-eslint/parser", // 新增
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["tsconfig.json"], // 新增
+    extraFileExtensions: [".vue"],
   },
-  plugins: ['vue'],
-  rules: {}
-}
+  plugins: ["vue"],
+  rules: { "@typescript-eslint/no-var-requires": 0 },
+};
